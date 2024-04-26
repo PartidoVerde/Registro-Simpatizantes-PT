88b90c80c3fef70bed5bb4f8d4996d7e72024842
Eduardo <aeduardoiran@gmail.com>
2024-04-26 08:48:07 -0700

    Se quito el boton registro del Login.jsx

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -79,7 +79,7 @@ function Login() {
                             <div className="flex flex-wrap">
                                 <label className="text-sm text-gray-200 cursor-pointer">
                                 </label>
-                                <p className="text-white mt-4"> Aun no tienes una cuenta? <Link to='/Register' className="text-sm text-blue-500 -200 hover:underline mt-4">Registrate</Link></p>
+                                {/*<p className="text-white mt-4"> Aun no tienes una cuenta? <Link to='/Register' className="text-sm text-blue-500 -200 hover:underline mt-4">Registrate</Link></p>*/}
                             </div>
                             <button className="bg-gradient-to-r from-indigo-500 to-blue-500 text-white font-bold py-2 px-4 rounded-md mt-4 hover:bg-indigo-600 hover:to-blue-600 transition ease-in-out duration-150" type="submit">Login</button>
                 </div>
